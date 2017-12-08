9bfd73fbe3fac8a47137478ed9de4bd038f8a5d1
Tiago Gasiba <tiago.gasiba@gmail.com>
2017-12-08 23:48:11 +0100

    added haskell mode

---
+++ .emacs.d/elpa/dante-20171207.1313/dante-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dante" "20171207.1313" "Development mode for Haskell" '((dash "2.13.0") (emacs "25.1") (f "0.19.0") (flycheck "0.30") (haskell-mode "13.14") (s "1.11.0")) :commit "a6b1933118b65b4c5d1ee5dd1a9ceb281af3db4c" :url "https://github.com/jyp/dante" :keywords '("haskell" "tools"))
